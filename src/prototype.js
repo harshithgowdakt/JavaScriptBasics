01772802724f5e50731b1841ef07a1bf18e6a11a
Harshith Gowda <harshithgowdakt@gmail.com>
2021-07-25 11:03:11 +0530

    updated prototype

--- src/prototype.js
+++ src/prototype.js
@@ -1,9 +1,10 @@
 function Particle() {
     this.x = 100;
     this.y = 99;
-    this.show = function () {
-        console.log("point ", this.x, this.y);
-    }
+}
+
+Particle.prototype.show = function () {
+    console.log("point ", this.x, this.y);
 }
 
 var p = new Particle();
