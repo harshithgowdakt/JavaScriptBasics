6ccf9fbc8ee6e9da9d710bee4d72b13461e6ff5a
Harshith Gowda <harshithgowdakt@gmail.com>
2021-07-25 12:04:17 +0530

    updated prototype

--- src/prototype.js
+++ src/prototype.js
@@ -16,3 +16,32 @@ console.log("p.__proto__.__proto__.__proto__", p.__proto__.__proto__.__proto__);
 console.log("Particle.prototype", Particle.prototype); //Particle prototype
 console.log("Particle.prototype.__proto__", Particle.prototype.__proto__); //Object.prototype
 console.log("Object.prototype", Object.prototype);
+
+
+//Performance difference object instance vs prototype
+function FunctionX() {
+    this.print = function () {
+        console.log("Calling print! ");
+    }
+}
+
+function FunctionY() {
+}
+
+FunctionY.prototype.print = function () {
+    console.log("Calling print!");
+}
+
+var xobjects = [];
+console.time('x');
+for (let i = 0; i < 2000000; i++) {
+    xobjects.push(new FunctionX());
+}
+console.timeEnd('x');
+
+var yobjects = [];
+console.time('y');
+for (let i = 0; i < 2000000; i++) {
+    yobjects.push(new FunctionY());
+}
+console.timeEnd('y');
