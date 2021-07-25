e6251fb79652ec46e4123fb7a3246dbf7b3cf6e2
Harshith Gowda <harshithgowdakt@gmail.com>
2021-07-25 11:22:29 +0530

    updated prototype

--- src/prototype.js
+++ src/prototype.js
@@ -8,3 +8,11 @@ Particle.prototype.show = function () {
 }
 
 var p = new Particle();
+
+console.log("p.__proto__", p.__proto__);
+console.log("p.__proto__.__proto__", p.__proto__.__proto__);
+console.log("p.__proto__.__proto__.__proto__", p.__proto__.__proto__.__proto__);
+
+console.log("Particle.prototype", Particle.prototype);
+console.log("Particle.prototype", Particle.prototype.__proto__);
+console.log("Object.prototype", Object.prototype);
