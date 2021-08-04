b7be0f779e3afdbdc14bdfe110b028f5c90d9aa2
Harshith Gowda <harshithgowdakt@gmail.com>
2021-08-04 23:06:09 +0530

    Wed  4 Aug 23:06:09 IST 2021

---
+++ src/os.js
@@ -0,0 +1,13 @@
+const os = require('os');
+
+
+console.log('cpu count:', os.cpus().length);
+console.log('arch:', os.arch());
+console.log('home-dir:', os.homedir());
+console.log('hostname:', os.hostname());
+console.log('uptime', os.uptime() / 60 / 60, 'hrs');
+console.log('use info:', os.userInfo());
+console.log('total mem:', os.totalmem() / 1024 / 1024 / 1024, "GB");
+console.log('get priority:', os.getPriority());
+console.log('load average:', os.loadavg());
+
--- src/path.js
+++ src/path.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-let rootDir = path.normalize(`${__dirname}/..`);
-console.log(rootDir);
-
+console.log(path.normalize(`${__dirname}/..`))
 console.log(path.parse(__filename));
 console.log(path.join(__dirname, 'child_process'));
