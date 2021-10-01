125723170a248c2196ed7e9e177759bfe69a4261
Harshith Gowda <harshithgowdakt@gmail.com>
2021-10-01 10:32:46 +0530

    password problem

--- src/interview/password.js
+++ src/interview/password.js
@@ -1,37 +1,27 @@
-function solve(k, ar, str) {
-    let localRetries = k;
-    let minTimeCounter = 0;
-    for (let i = 0; i < ar.length; i++) {
-        localRetries -= 1;
-        if (str === ar[i]) {
-            minTimeCounter += 1;
-            break;
-        } else if (localRetries === 0) {
-            localRetries = k;
-            minTimeCounter += 6;
-        } else {
-            minTimeCounter += 1;
+function getMinandMaxTime(numberOfRetries, passwords, correctPassword) {
+    let retries = numberOfRetries;
+    let minTime = 0;
+    let maxTime = 0;
+    let found = false;
+    for (let i = 0; i < passwords.length; i++) {
+        if (!found) {
+            minTime += 1;
         }
-    }
-
-    let maxTimeCounter = 0;
-    localRetries = k;
-    for (let i = 0; i < ar.length; i++) {
-        localRetries -= 1;
-        if (str === ar[i]) {
-            maxTimeCounter += 1;
-        } else if (str.length < ar[i].length) {
+        maxTime += 1;
+        if (correctPassword === passwords[i]) {
+            found = true;
+        } else if (retries === 0) {
+            retries = numberOfRetries;
+            if (!found) {
+                minTime += 5;
+            }
+            maxTime += 5;
+        } else if (correctPassword.length < passwords[i].length) {
             break;
-        } else if (localRetries <= 0) {
-            localRetries = k;
-            maxTimeCounter += 6;
-        } else {
-            maxTimeCounter += 1;
         }
+        retries -= 1;
     }
-    return [minTimeCounter, maxTimeCounter];
+    return [minTime, maxTime];
 }
 
-solve(1, ["aa1", "bbb", "ccc", "ddd", "qqq"], "aa1")
-
-console.log(solve(1, ["awd", "aa1", "bbb", "ccc", "ddd", "qqq", "qawdw"], "aa1"));
+console.log(getMinandMaxTime(1, ["aa1", "bbb", "ccc", "ddd", "qqq", "qawdw"], "aa1"));
