a5f603f135af4f199089b1129cf60948f105a715
Harshith Gowda <harshithgowdakt@gmail.com>
2021-10-05 14:29:17 +0530

    Tue  5 Oct 14:29:17 IST 2021

---
+++ src/var.js
@@ -0,0 +1,46 @@
+// console.log(x);
+// console.log(y);
+// var x = 10;
+// let y = 10;
+
+
+// var x = 10;
+// function z() {
+//     let x = 20;
+//     console.log(this.x);
+//     console.log(x);
+// }
+// z();
+
+
+
+// var x = 30;
+// var testObj = {
+//     x: 20,
+//     print: function () {
+//         console.log(x);
+//         console.log(this.x)
+//     }
+// }
+
+// function print() {
+//     console.log(x)
+//     console.log(this.x);
+// }
+
+// testObj.print()
+// print();
+
+// function q() {
+//     var w = 10
+// }
+// q();
+// console.log(w);
+
+
+// const arr = [1, 2, 3, 4]
+// for (let i = 0; i < arr.length; i++) {
+//     setTimeout(function () {
+//         console.log(i, arr[i], arr.length);
+//     }, 1000);
+// }
