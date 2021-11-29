24b9032ece748dd17761442568c2cf54daa91ef0
Harshith Gowda <harshithgowdakt@gmail.com>
2021-11-29 21:38:55 +0530

    :added let and var example

--- src/let.js
+++ src/let.js
@@ -1,13 +1,43 @@
-console.log("x", x);
-let x = 10;
+/**
+ * var is function scope and let is block scope
+ * hoisting = var show undefined and let through reference error
+ * can't redclare let variables, but var variables can be redeclared
+ * var variables are attached to global abject
+ */
 
 
+// console.log("x", x);
+// let x = 10;
 
+var funcs = [];
+// let's create 3 functions
+for (var i = 0; i < 3; i++) {
+    // and store them in funcs
+    funcs[i] = function () {
+        // each should log its value.
+        console.log("My value: " + i);
+    }
+}
 
-console.log("a")
+for (var j = 0; j < 3; j++) {
+    // and now let's run each one to see
+    funcs[j]();
+}
+// My Value : 3 for all the functions
 
-setTimeout(()=>{
-    console.log("b")
-},1000)
+// let example
 
-console.log('c')
+var funcs = [];
+// let's create 3 functions
+for (let i = 0; i < 3; i++) {
+    // and store them in funcs
+    funcs[i] = function () {
+        // each should log its value.
+        console.log("My value: " + i);
+    }
+}
+
+for (var j = 0; j < 3; j++) {
+    // and now let's run each one to see
+    funcs[j]();
+}
