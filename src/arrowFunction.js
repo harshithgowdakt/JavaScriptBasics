2fe627c5a10750aed95e8dd80e2c905df592b121
Harshith Gowda <harshithgowdakt@gmail.com>
2021-11-29 22:41:59 +0530

    Mon 29 Nov 22:41:59 IST 2021: arrow function vs normal function

--- src/arrowFunction.js
+++ src/arrowFunction.js
@@ -1,3 +1,14 @@
+
+/**
+1. Lexical this and arguments
+
+Arrow functions don't have their own this or arguments binding.
+Instead, those identifiers are resolved in the lexical scope like
+any other variable. That means that inside an arrow function,
+this and arguments refer to the values of this and arguments in the
+environment the arrow function is defined in.
+ */
+
 let person1 = {
     firstname: 'Harshith',
     lastname: 'Gowda',
@@ -16,3 +27,20 @@ let person2 = {
         console.log(`Hello ${this.firstname}`);
     }
 }
+person2.sayHello();
+
+let arrowFunctionExample = () => {
+    for (let index = 0; index < arguments.length; index++) {
+        const arg = arguments[index];
+        console.log(arg);
+    }
+}
+arrowFunctionExample(1, 2)
+
+let normalFunctionExample = function () {
+    for (let index = 0; index < arguments.length; index++) {
+        const arg = arguments[index];
+        console.log(arg);
+    }
+}
+normalFunctionExample(1, 2)
