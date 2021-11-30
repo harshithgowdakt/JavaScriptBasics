eca0c5fae95d31da9099b900e1d62caee74317a9
Harshith Gowda <harshithgowdakt@gmail.com>
2021-11-30 22:08:35 +0530

    Tue 30 Nov 22:08:35 IST 2021: array, call, apply and bind and object

--- src/Array.js
+++ src/Array.js
@@ -11,3 +11,35 @@ for (let [key, value] of Object.entries(obj).sort((a, b) => b[1] - a[1])) {
     sortedObject[key] = value;
 }
 console.log(sortedObject);
+
+let empty = [1, 2, 3];
+let length = empty.length
+
+for (let index = 0; index < empty.length; index++) {
+    console.log(index);
+    console.log("popped ele", empty.pop());
+}
+
+empty.length = 0;
+console.log("emptied array", empty);
+
+
+
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+let removed = months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months, removed);
+
+
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(2));
+console.log(animals.slice(2, 4));
+console.log(animals.slice(1, 5));
+console.log(animals.slice(-2));
+console.log(animals.slice(-2), animals);
--- src/object.js
+++ src/object.js
@@ -1,63 +1,69 @@
-let fs = require('fs');
+// let fs = require('fs');
 
-// method 1
-var myCar1 = new Object();
-myCar.make = 'Ford';
-myCar.model = 'Mustang';
-myCar.year = 1969;
+// // method 1
+// var myCar1 = new Object();
+// myCar.make = 'Ford';
+// myCar.model = 'Mustang';
+// myCar.year = 1969;
 
-console.log(myCar1);
+// console.log(myCar1);
 
-//method 2
-var myCar2 = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969
-};
+// //method 2
+// var myCar2 = {
+//     make: 'Ford',
+//     model: 'Mustang',
+//     year: 1969
+// };
 
-console.log(myCar2);
+// console.log(myCar2);
 
-//method 3 
-var myCar3 = {};
-myCar['make'] = 'Ford';
-myCar['model'] = 'Mustang';
-myCar['year'] = 1969;
-console.log(myCar3);
+// //method 3 
+// var myCar3 = {};
+// myCar['make'] = 'Ford';
+// myCar['model'] = 'Mustang';
+// myCar['year'] = 1969;
+// console.log(myCar3);
 
-//special key names
-var obj = {};
-obj['hello world'] = 'hello, world';
-obj['hello-world'] = 'hello, world';
-obj.hello_world = 'hello, world';
-obj['2'] = 'hello, world';
-console.log(obj);
+// //special key names
+// var obj = {};
+// obj['hello world'] = 'hello, world';
+// obj['hello-world'] = 'hello, world';
+// obj.hello_world = 'hello, world';
+// obj['2'] = 'hello, world';
+// console.log(obj);
 
-//enumarating over objects
+// //enumarating over objects
 
-//method 1
-for (const key in obj) {
-    console.log(obj[key]);
-}
+// //method 1
+// for (const key in obj) {
+//     console.log(obj[key]);
+// }
 
-// method 2
-for (const value of obj) {
-    console.log(value);
-}
+// // method 2
+// for (const value of obj) {
+//     console.log(value);
+// }
 
-//method 3
-for (const [key, value] of Object.entries(obj)) {
-    console.log(`${key} : ${value}`)
-}
+// //method 3
+// for (const [key, value] of Object.entries(obj)) {
+//     console.log(`${key} : ${value}`)
+// }
 
 
-let x = {
-    a: 10,
-    b: 10
-}
+// let x = {
+//     a: 10,
+//     b: 10
+// }
 
-let y = {
-    a: 10,
-    b: 10
-}
+// let y = {
+//     a: 10,
+//     b: 10
+// }
 
-console.log(x == y);
+// console.log(x == y);
+
+
+let source = { a: 10, b: 20 };
+let target = {};
+target = Object.create();
+console.log(target === source);
