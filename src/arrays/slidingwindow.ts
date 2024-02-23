a92b9b11af3f2d9f1405890a360d9a4d285cfd0a
Harshith Gowda <harshithgowdakt@gmail.com>
2024-02-23 21:22:01 +0530

    sliding window : sums of all sub arrays

--- src/arrays/slidingwindow.ts
+++ src/arrays/slidingwindow.ts
@@ -18,13 +18,36 @@ function maxSumSubArray(arr: number[], k: number) {
   return maxSum;
 }
 
+function sumOfSubArrays(arr: number[], k: number) {
+  if (arr.length < k) return -1;
+  let sums = [];
+  let firstSum = 0;
+  for (let i = 0; i < k; i++) {
+    firstSum += arr[i];
+  }
+  sums.push(firstSum);
+
+  for (let j = 1; j <= arr.length - k; j++) {
+    firstSum = firstSum - arr[j - 1] + arr[j + k - 1];
+    sums.push(firstSum);
+  }
+  return sums;
+}
+
 let arr = [1, 4, 2, 10, 2, 3, 1, 0, 20];
 
 let k = 3;
-let result = maxSumSubArray(arr, k);
+let maxSum = maxSumSubArray(arr, k);
 
-if (result == -1) {
+if (maxSum == -1) {
   console.log("Invalid input");
 } else {
-  console.log("Maximum sum of subarray of size ", result);
+  console.log("Maximum sum of subarray of size ", maxSum);
+}
+
+let sums = sumOfSubArrays(arr, k);
+if (sums == -1) {
+  console.log("Invalid input");
+} else {
+  console.log("sum of subarrays of size ", sums);
 }
