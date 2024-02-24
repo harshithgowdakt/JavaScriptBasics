0f687d41708d1b1e6a90e6f40d1988437a7e5e99
Harshith Gowda <harshithgowdakt@gmail.com>
2024-02-24 14:38:58 +0530

    sliding window : lengthOfLongestSubstring

--- src/arrays/slidingwindow.ts
+++ src/arrays/slidingwindow.ts
@@ -34,6 +34,9 @@ function sumOfSubArrays(arr: number[], k: number) {
   return sums;
 }
 
+// Given an array of positive integers and a target sum s,
+// find the length of the smallest contiguous subarray whose sum is greater than or equal to s.
+// If no such subarray exists, return 0.
 function smallestSubarrayWithGivenSum(arr: number[], s: number): number {
   let minLength = Number.MAX_VALUE;
   let windowSum = 0;
@@ -47,6 +50,34 @@ function smallestSubarrayWithGivenSum(arr: number[], s: number): number {
     }
   }
   return minLength === Number.MAX_VALUE ? 0 : minLength;
+}
+
+/**
+ * The problem of finding the length of the longest substring without repeating 
+ * characters in a given string is a classic example of a sliding window technique application.
+ * The goal is to identify the longest contiguous segment of the string that contains unique characters, 
+ * meaning no character appears more than once within that segment.
+
+  Understanding the Problem
+  Given a string s, you need to find the maximum length of a substring that does not contain any repeating characters. For example:
+
+  If s = "abcabcbb", the answer is 3, because the longest substring without repeating characters is "abc", which occurs twice.
+  If s = "bbbbb", the answer is 1, since the longest substring without repeating characters is any single character.
+  If s = "pwwkew", the answer is 3, because the longest substring without repeating characters is "wke".
+ */
+
+function lengthOfLongestSubstring(s: string) {
+  let len = 0;
+  let start = 0;
+  let charMap = {};
+  for (let end = 0; end < s.length; end++) {
+    if (!!charMap[s.charAt(end)]) {
+      start = Math.max(charMap[s.charAt(end)] + 1, start);
+    }
+    charMap[s.charAt(end)] = end;
+    len = Math.max(end - start + 1, len);
+  }
+  return len;
 }
 
 let arr = [1, 4, 2, 10, 2, 3, 1, 0, 20];
@@ -67,3 +98,6 @@ if (sums == -1) {
 }
 
 console.log(smallestSubarrayWithGivenSum(arr, 7));
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbbbbbbbbbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
