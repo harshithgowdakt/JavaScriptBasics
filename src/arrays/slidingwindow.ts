b993727eb4b87766426e7d5e4941a68e12b6c5ef
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-03 22:55:32 +0530

    count all palidrome

--- src/arrays/slidingwindow.ts
+++ src/arrays/slidingwindow.ts
@@ -433,3 +433,40 @@ const minWinPermutation = function (s: string, t: string) {
   }
   return minString;
 };
+
+/**
+Given a string s, return the number of palindromic substrings in it.
+A string is a palindrome when it reads the same backward as forward.
+A substring is a contiguous sequence of characters within the string.
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa". 
+*/
+
+var countSubstrings = function (s) {
+  let res = 0;
+
+  const countPali = function (s, l, r) {
+    let res = 0;
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++;
+      res++;
+    }
+    return res;
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    res += countPali(s, i, i);
+    res += countPali(s, i, i + 1);
+  }
+  return res;
+};
