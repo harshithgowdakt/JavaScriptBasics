2390db2c320937190283850906042e38e9566f6b
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-04 19:37:03 +0530

    max area

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-const minWinPermutation = function (s, t) {
-  let targetCharToCountMap = {};
-  let stringCharToCountMap = {};
+var maxArea = function (height) {
+  let start = 0,
+    end = height.length - 1,
+    res = 0;
 
-  let start = 0;
-  let minLen = Number.MAX_VALUE;
-  let minString = "";
-
-  const isMapGreathanOrEqual = function (a, b) {
-    let keys = Object.keys(targetCharToCountMap);
-    for (let i = 0; i < keys.length; i++) {
-      if (b[keys[i]] === undefined) return false;
-      else if (b[keys[i]] < a[keys[i]]) return false;
-    }
-    return true;
-  };
-
-  for (let i = 0; i < t.length; i++) {
-    targetCharToCountMap[t[i]] = (targetCharToCountMap[t[i]] || 0) + 1;
-    stringCharToCountMap[s[i]] = (stringCharToCountMap[s[i]] || 0) + 1;
-  }
-
-  if (isMapGreathanOrEqual(targetCharToCountMap, stringCharToCountMap))
-    return s.substring(start, t.length);
-
-  for (let end = t.length; end < s.length; end++) {
-    stringCharToCountMap[s[end]] = (stringCharToCountMap[s[end]] || 0) + 1;
-    while (isMapGreathanOrEqual(targetCharToCountMap, stringCharToCountMap)) {
-      if (end - start + 1 < minLen) {
-        minLen = end - start + 1;
-        minString = s.substring(start, end + 1);
-      }
-      stringCharToCountMap[s[start]]--;
+  while (start < end) {
+    let area = (start - end) * Math.min(height[start], height[end]);
+    res = Math.max(area, res);
+    if (start < end) {
       start++;
+    } else {
+      end--;
     }
   }
-  return minString;
+  return res;
 };
 
-console.log(minWinPermutation("ADOBECODEBANC", "ABC"));
+console.log(maxArea([(2, 3, 4, 5, 18, 17, 6)]));
--- src/arrays/slidingwindow.ts
+++ src/arrays/slidingwindow.ts
@@ -470,3 +470,33 @@ var countSubstrings = function (s) {
   }
   return res;
 };
+
+/**
+  You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints
+  of the ith line are (i, 0) and (i, height[i]).
+  Find two lines that together with the x-axis form a container, such that the container contains the most water.
+  Return the maximum amount of water a container can store.
+  Notice that you may not slant the container.
+
+  Input: height = [1,8,6,2,5,4,8,3,7]
+  Output: 49
+  Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+  In this case, the max area of water (blue section) the container can contain is 49.
+ */
+
+var maxArea = function (height) {
+  let start = 0,
+    end = height.length - 1,
+    res = 0;
+
+  while (start < end) {
+    let area = (end - start) * Math.min(height[start], height[end]);
+    res = Math.max(area, res);
+    if (height[start] < height[end]) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return res;
+};
