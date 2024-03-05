47a17d4951378614c2e9505ac22236fabe60303d
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-05 21:37:03 +0530

    two pointer problems

--- src/arrays/slidingwindow.ts
+++ src/arrays/slidingwindow.ts
@@ -500,3 +500,85 @@ var maxArea = function (height) {
   }
   return res;
 };
+
+/**
+ Given an integer array nums, move all 0's to the end of it while maintaining the 
+ relative order of the non-zero elements.
+ Note that you must do this in-place without making a copy of the array.
+  Example 1:
+
+  Input: nums = [0,1,0,3,12]
+  Output: [1,3,12,0,0]
+  Example 2:
+
+  Input: nums = [0]
+  Output: [0]
+ */
+const moveZeroes = function (nums) {
+  if (nums.length <= 1) return nums;
+  let l = 0,
+    r = 1;
+  while (r < nums.length) {
+    if (nums[l] != 0) {
+      l++;
+      r++;
+    } else if (nums[r] == 0) {
+      r++;
+    } else {
+      let temp = nums[r];
+      nums[r] = nums[l];
+      nums[l] = temp;
+      l++;
+      r++;
+    }
+  }
+  return nums;
+};
+
+/**
+  Remove Duplicates from Sorted Array
+  Given an integer array nums sorted in non-decreasing order,
+  remove the duplicates in-place such that each unique element appears only once.
+  The relative order of the elements should be kept the same. Then return the number 
+  of unique elements in nums.
+
+  Consider the number of unique elements of nums to be k, to get accepted, 
+  you need to do the following things:
+
+  Change the array nums such that the first k elements of nums contain the unique elements
+  in the order they were present in nums initially. The remaining elements of nums are not 
+  important as well as the size of nums.
+
+  Example 1:
+
+  Input: nums = [1,1,2]
+  Output: 2, nums = [1,2,_]
+  Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+  It does not matter what you leave beyond the returned k (hence they are underscores).
+  Example 2:
+
+  Input: nums = [0,1,2,3,4,2,2,3,3,4]
+  Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+  Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+  It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+
+var removeDuplicates = function (nums) {
+  //base case
+  if (nums.length < 2) return nums.length;
+
+  //pointers
+  let l = 0,
+    r = 1;
+
+  // remove duplicates
+  while (r < nums.length) {
+    if (nums[l] != nums[r]) {
+      l++;
+      nums[l] = nums[r];
+    }
+    r++;
+  }
+  //return length
+  return l + 1;
+};
