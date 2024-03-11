d827003ba53a5fe3944e447693fef812499ca8e0
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-11 19:39:43 +0530

    union of array

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-/**
-    Given two integer arrays nums1 and nums2, 
-    return an array of their intersection. 
-    Each element in the result must be unique and
-    you may return the result in any order.
+function union(nums1, nums2) {
+  let res = [];
+  let i = 0,
+    j = 0;
 
-    Example 1:
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] <= nums2[j]) {
+      if (res.length == 0 || res[res.length - 1] !== nums1[i])
+        res.push(nums1[i]);
+      i++;
+    } else {
+      if (res.length == 0 || res[res.length - 1] !== nums2[j])
+        res.push(nums2[j]);
+      j++;
+    }
+  }
 
-    Input: nums1 = [1,2,2,1], nums2 = [2,2]
-    Output: [2]
-    Example 2:
+  while (i < nums1.length) {
+    if (res.length == 0 || res[res.length - 1] !== nums1[i]) res.push(nums1[i]);
+    i++;
+  }
 
-    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-    Output: [9,4]
-    Explanation: [4,9] is also accepted.
-*/
+  while (j < nums2.length) {
+    if (res.length == 0 || res[res.length - 1] !== nums2[j]) res.push(nums2[j]);
+    j++;
+  }
 
-function interSectionOfArray(nums1, nums2) {
-  let set1 = new Set(nums1);
-  let set2 = new Set(nums2);
-  return Array.from(new Set([...set1].filter((num) => set2.has(num))));
+  return res;
 }
-
-console.log(interSectionOfArray([4, 9, 5], [9, 4, 9, 8, 4]));
+console.log(union([1, 2, 3, 4, 5], [1, 2, 3, 6, 7]));
--- src/arrays/unionOfArray.js
+++ src/arrays/unionOfArray.js
@@ -18,3 +18,33 @@ function unionOfArray(nums1, nums2) {
   let set2 = new Set(nums2);
   return Array.from(new Set([...set1, ...set2]));
 }
+
+function union(nums1, nums2) {
+  let res = [];
+  let i = 0,
+    j = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] <= nums2[j]) {
+      if (res.length > 0 && res[res.length - 1] !== nums1[i])
+        res.push(nums1[i]);
+      i++;
+    } else {
+      if (res.length > 0 && res[res.length - 1] !== nums2[j])
+        res.push(nums2[j]);
+      j++;
+    }
+  }
+
+  while (i < nums1.length) {
+    if (res.length > 0 && res[res.length - 1] !== nums1[i]) res.push(nums1[i]);
+    i++;
+  }
+
+  while (j < nums2.length) {
+    if (res.length > 0 && res[res.length - 1] !== nums2[j]) res.push(nums2[j]);
+    j++;
+  }
+
+  return res;
+}
