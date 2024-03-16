045d64e4e82d5b579bf54bacddb10079fc07d1d7
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-16 18:13:02 +0530

    rearrange numbers not equal

--- src/arrays/rearrangeNumber.js
+++ src/arrays/rearrangeNumber.js
@@ -51,3 +51,89 @@ const rearrangeTwoPointers = function (nums) {
   }
   return ans;
 };
+
+/**
+
+There’s an array ‘A’ of size ‘N’ with positive and negative elements (not necessarily equal). 
+
+Example 1:
+
+Input:
+arr[] = {1,2,-4,-5,3,4}, N = 6
+Output:
+1 -4 2 -5 3 4
+
+Explanation: 
+
+Positive elements = 1,2
+Negative elements = -4,-5
+To maintain relative ordering, 1 must occur before 2, and -4 must occur before -5.
+Leftover positive elements are 3 and 4 which are then placed at the end of the array.
+*/
+
+[1, 2, 3, 4][(-4, -5)];
+[1, -4, 2, -5, 3, 4];
+
+const rearrangeNotEqual = function (nums) {
+  let ans = [],
+    positives = [],
+    negetives = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) negetives.push(nums[i]);
+    else positives.push(nums[i]);
+  }
+
+  if (negetives.length < positives.length) {
+    for (let i = 0; i < negetives.length; i++) {
+      ans[2 * i] = positives[i];
+      ans[2 * i + 1] = negetives[i];
+    }
+    let index = negetives.length * 2;
+    for (let i = negetives.length; i < positives.length; i++) {
+      ans[index] = positives[i];
+      index++;
+    }
+  } else {
+    for (let i = 0; i < positives.length; i++) {
+      ans[2 * i] = positives[i];
+      ans[2 * i + 1] = negetives[i];
+    }
+
+    let index = positives.length * 2;
+    for (let i = positives.length; i < negetives.length; i++) {
+      ans[index] = negetives[i];
+      index++;
+    }
+  }
+
+  return ans;
+};
+
+function rearrangeOptimozed(nums) {
+  let i = 0,
+    j = 0,
+    k = 0,
+    positives = [],
+    negetives = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) negetives.push(nums[i]);
+    else positives.push(nums[i]);
+  }
+
+  while (i < positives.length && j < negetives.length) {
+    nums[k++] = positives[i++];
+    nums[k++] = negetives[j++];
+  }
+
+  while (i < positives.length) {
+    nums[k++] = positives[i++];
+  }
+
+  while (j < negetives.length) {
+    nums[k++] = negetives[j++];
+  }
+
+  return nums;
+}
