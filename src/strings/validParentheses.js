250f4ba3bb8fc891f81907d4d2a40109c702143d
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-20 19:41:26 +0530

    valid parentheses

--- src/strings/validParentheses.js
+++ src/strings/validParentheses.js
@@ -1,3 +1,27 @@
+/**
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+*/
+
 function validParentheses(s) {
   // create a map of open parentheses to closed parentheses
   let parenthesesMap = new Map();
@@ -19,4 +43,3 @@ function validParentheses(s) {
   }
   return st.length === 0;
 }
-console.log(validParentheses("()[]{}}"));
