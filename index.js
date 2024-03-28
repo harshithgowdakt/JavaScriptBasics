9398a5c756d4fbf66a0a414633487959ecea2e69
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-28 21:43:58 +0530

    remove element

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-var maxSubarrayLength = function (nums, k) {
+var removeElement = function (nums, val) {
   let L = 0,
     R = 0;
-  let frequencyMap = new Map();
-
-  let maxLen = 0;
-
   while (R < nums.length) {
-    frequencyMap.set(nums[R], (frequencyMap.get(nums[R]) ?? 0) + 1);
-
-    while (Math.max(...Array.from(frequencyMap.values())) > k) {
-      frequencyMap.set(nums[L], (frequencyMap.get(nums[L]) ?? 0) - 1);
-      if (frequencyMap.get(nums[L]) === 0) frequencyMap.delete(nums[L]);
+    if (nums[L] != val) {
+      L++;
+      R++;
+    } else if (nums[R] == val) {
+      R++;
+    } else {
+      let temp = nums[R];
+      nums[R] = nums[L];
+      nums[L] = temp;
+      R++;
       L++;
     }
-
-    maxLen = Math.max(maxLen, R - L + 1);
-    R++;
   }
-  return maxLen;
+  return L;
 };
-
-console.log(maxSubarrayLength([1, 2, 1, 2, 1, 2, 1, 2], 1));
