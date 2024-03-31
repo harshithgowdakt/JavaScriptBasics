7ce260f02f4d1b4ea5848b3c2c84763b413dac85
Harshith Gowda <harshithgowdakt@gmail.com>
2024-03-31 17:39:37 +0530

    missing number

--- src/arrays/missingNumber.js
+++ src/arrays/missingNumber.js
@@ -9,20 +9,18 @@ function findMissingNumber(arr, N) {
   return totalSum - arrSum;
 }
 
-function findMissingNumber(arr, N) {
-  let xor_full = 1; // Start XORing from 1 to N
-  for (let i = 2; i <= N; i++) {
-    xor_full = xor_full ^ i;
+var missingNumber = function (nums) {
+  let xorFull = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    xorFull ^= i;
   }
 
-  let xor_arr = arr[0]; // Start XORing all elements in the array
-  for (let i = 1; i < N - 1; i++) {
-    xor_arr = xor_arr ^ arr[i];
+  let xorArr = 0;
+  for (let i = 0; i < nums.length; i++) {
+    xorArr ^= nums[i];
   }
-
-  // XORing xor_full and xor_arr gives the missing number
-  return xor_full ^ xor_arr;
-}
+  return xorArr ^ xorFull;
+};
 
 // Example usage
 const arr = [1, 2, 4, 6, 3]; // N is 6, array size is N-1 = 5
