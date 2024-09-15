c441e51639800e6bd8610762c8930e7c2d794353
Harshith Gowda <harshithgowdakt@gmail.com>
2024-09-15 19:17:08 +0530

    var and settimeoout

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-class Solution {
-    // Function to minimize the cost of reducing the heights.
-    minimizeCost(height, n, k) {
-        let dp = Array(height.length - 1).fill(0);
-
-        for (let i = 1; i < height.length - 1; i++) {
-            let minJump = Infinity;
-            for (let j = i; j <= k; j++) {
-                if (i - j >= 0) {
-                    let jump = dp[i - j] + Math.abs(height[i] - height[i - j]);
-                    minJump = Math.min(jump, minJump);
-                }
-            }
-            dp[i] = minJump;
-        }
-        return dp[height.length - 1];
-    }
+function outerFunction(outerVariable) {
+    return function innerFunction(innerVariable) {
+        console.log(`Outer Variable: ${outerVariable}`);
+        console.log(`Inner Variable: ${innerVariable}`);
+    };
 }
+
+const newFunction = outerFunction('outside');
+newFunction('inside');
---
+++ src/hoisting.js
@@ -0,0 +1,3 @@
+console.log(a); // undefined (hoisted, but not yet initialized)
+var a = 10;
+console.log(a); // 10
