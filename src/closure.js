ff4bb4d5303ab757ec177b52c4e61b0f2092d623
Harshith Gowda <harshithgowdakt@gmail.com>
2024-09-15 19:25:07 +0530

    closuers

--- src/closure.js
+++ src/closure.js
@@ -16,3 +16,27 @@ console.log(counter2());
 console.log(counter2());
 console.log(counter2());
 console.log(counter2());
+
+
+
+function createMultiplayers(multiplyer) {
+    return function (number) {
+        return multiplyer * number;
+    }
+}
+
+
+let double = createMultiplayers(2);
+console.log(double(2));
+console.log(double(3));
+console.log(double(4));
+console.log(double(5));
+
+
+let triple = createMultiplayers(3);
+console.log(triple(2));
+console.log(triple(3));
+console.log(triple(4));
+console.log(triple(5));
+console.log(triple(6));
+
