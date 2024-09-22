b09e53c9810078bdb83938689f3f8d6dcb8d9a8b
Harshith Gowda <harshithgowdakt@gmail.com>
2024-09-22 15:53:44 +0530

    closuers

---
+++ src/sanasInterview/domian.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+
+function isValidDomain(email) {
+    let splits = email.split("@");
+
+    if (splits.length > 2 || splits.length <= 1) {
+        throw new Error("this is not a valid domain");
+    }
+
+    let secondSplit = splits[1].split(".");
+
+    if (secondSplit.length > 2 || secondSplit.length <= 1) {
+        throw new Error("this is not a valid domain");
+    }
+
+    if (secondSplit[1] == "com") {
+        return splits[1];
+    }
+
+    throw new Error("this is not valid domain");
+}
+
+
+app.get('/', (req, res) => {
+    try {
+        let domain = isValidDomain(req.body.email);
+        res.send(`The valid domain is ${domain}`);
+    } catch (error) {
+        res.send("This is not a valid domain");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
