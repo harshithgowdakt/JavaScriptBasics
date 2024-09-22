085a2d3c44e44ef5aad0eada43ec5b0803f2d2f9
Harshith Gowda <harshithgowdakt@gmail.com>
2024-09-22 22:10:08 +0530

    sanas interview

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-function createFunctions() {
-    let arr = [];
-    for (let i = 0; i < 3; i++) {
-        arr.push(function () {
-            console.log(i); // The variable i is shared across all closures
-        });
+const person = {
+    name: 'Ajay',
+    greet: () => {
+        console.log(this); // Logs the `person` object
+        console.log(this.name); // Logs 'Ajay'
+    },
+    welcome: function () {
+        setTimeout(function () {
+            console.log("this in regular object", this); // golbal object
+        }, 1000);
+    },
+    welcome2: function () {
+        setTimeout(() => {
+            console.log("this in arrow object", this); //lexical scope;
+        }, 1000);
     }
-    return arr;
-}
+};
 
-const f = createFunctions();
-f[0](); // 0
-f[1](); // 1
-f[2](); // 2
+// person.greet(); // Calling the method greet
+person.welcome();
+person.
--- src/this.js
+++ src/this.js
@@ -1,13 +1,24 @@
 const person = {
     name: 'Ajay',
-    greet:  ()=> {
+    greet: () => {
         console.log(this); // Logs the `person` object
         console.log(this.name); // Logs 'Ajay'
+    },
+    welcome: function () {
+        setTimeout(function () {
+            console.log("this in regular object", this); // golbal object
+        }, 1000);
+    },
+    welcome2: function () {
+        setTimeout(() => {
+            console.log("this in arrow object", this); //lexical scope;
+        }, 1000);
     }
 };
 
 person.greet(); // Calling the method greet
-
+person.welcome(); // global object
+person.welcome2(); // object
 
 
 function Person(name) {
