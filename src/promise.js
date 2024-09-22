7465f218aa0a88982183bce4a6a2659087651dca
Harshith Gowda <harshithgowdakt@gmail.com>
2024-09-23 00:02:12 +0530

    sanas interview

--- src/promise.js
+++ src/promise.js
@@ -27,3 +27,20 @@ promise1.then(res => {
     console.log(res)
 })
 console.log('end');
+
+
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Data received"), 1000);
+});
+
+promise2
+    .then((data) => {
+        console.log(data); // "Data received"
+        return "Processed data";
+    })
+    .then((processedData) => {
+        console.log(processedData); // "Processed data"
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
