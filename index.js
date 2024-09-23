c1938ae1afc4f7450c6c16f2c1e75b8c2e10ccd4
Harshith Gowda <harshithgowdakt@gmail.com>
2024-09-23 14:42:34 +0530

    sanas interview

--- index.js
+++
@@ -1,8 +0,0 @@
-Promise.resolve("Success")
-    .then(() => {
-        throw new Error("Something went wrong");
-    });
-// No .catch() present
-
-console.log("This still runs, but the error goes unhandled.");
-
